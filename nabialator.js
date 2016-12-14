4f16d15bfa3e5e59e6936f731a7f102901c036e2
puldzian <puldzian@gmail.com>
2016-12-14 20:36:32 +0100

    dodałem troche bajerów

--- nabialator.js
+++ nabialator.js
@@ -1,14 +1,14 @@
 /*global $*/
 
-var status = "nie ma jeszcze statusu",
-    robie = ["śpię", "siedzę", "stoję", "liczcie pieniądze", "robię kupę", "masuję głowę", "piję herbatę"],
-    naczym = ["na tapczanie", "na klawiaturze", "na wersalce", "na chuju", "na towarze", "na lodowisku", "na zleceniu", "na umowie o dzieło", "na zeszycie", "na fajce", "na google maps", "na serwerze pocztowym", "na panoramie firm", "na telefonie"],
-    jakco = ["jak jakiś skrót", "jak galareta", "jak kawał chuja", "jak ryba", "jak jajo", "jak herbata", "jak liczydło", "jak dziki pies", "jak wampirze dziecię", "jak kurczak", "jak policjant", "jak ciągnik", "jak piwosz"],
-    nazywamsie = ["moja godność to", "mam na imię", "nazywam się", "nie mam wątpliwości, że moje nazwisko brzmi", "mój gatunek to", "ten awatar nazywa się", "moja klasa obiektu to", "dzieci mówią do mnie per", "w dokumentach wpisano mi", "mówią do mnie", "moja nazwa usługi to", "możesz nazywać mnie", "nazwa tego pliku to", "karzdy katolik to", "godność mojej godności to", "moje imię i nazwisko brzmi", "moja nazwa to", "mój pseudonim artystyczny to"],
-    nazwa = ["ciapaja", "zębatka", "maszyna z guwna", "karabinek", "Jeremiasz", "ojciec Jeremiasz", "mieszadełko", "cieplutkie pozdrowienia", "ściskacz", "telefon Sony Xperia L", "karta pamięci", "ciągniczek", "ciepła zupka i zimne spodnie", "sopel lodu", "buty do tańca", "kolorowy jarmuż"],
-    wczym = ["w majonezie", "w sklepie", "w dupie", "w kabelku", "w budce strarznika", "w kartoflu", "w worku kartofli", "w niemrawej okolicy", "w klepsydrze", "w egipskim kurorcie", "w Polsce", "w twojej dupie", "w dupie twojej starej", "w tafli lodu", "u łukasza", "na ruczaju", "na katapulcie", "w przychodni lekarskiej", "w świni", "w bibliotece w radomiu"],
-    jestem = ["przebywam obecnie", "spendzam czas", "jestem zameldowany", "zaznaczam okienko", "zarzywam urlopu", "odpoczywam", "jem obiad", "buduje domek", "gram w katapulte", ""],
-    czesc = ["cześć", "witam cieplutko", "witam serdecznie", "dzień dobry", "zapraszam do środka", "proszę nie stać w progu", "dobry wieczur", "szanowni państwo", "cześć, witajcie", "witajcie", "jak się macie"];
+var status = "nie mam jeszcze statusu",
+    robie = ["śpie", "siedze", "stoje", "licze walute", "robie kupe", "masuje głowe", "pije sobie herbate", "jem kanapke", "odpoczywam", "zarzywam odpoczynku", "dobrze sie bawie", "trenuje", "martwie sie", "ładuje telefon", "sprawdzam poczte", "sprawdzam powiadomienia", "jem zupe", "zalewam vifona", "tresuje pająka", "podpinam ładowarke", "oglądam serial", "jem jajo", "jem majonez", "czytam ebuka", "czytam statusy", "patrze w nabiau", "co ja robie", "gnije", "pokrywam sie pleśniom", "grzybieje", "martwie sie bardzo", "myje sie", "obcinam paznokcie", "co ja w ogule robie", "jakby siedze", "jakby lerze", "robie coś", "jakby odpoczywam", "tak sobie przysiadłem", "mam wolnom chwilke", "skradam sie powoli", "rozkładam posłanie", "toleruje ciebie", "oglondam komputer", "patrze na pieniondze", "czytam smsy", "oglondam selfiki", "boli mnie dupa", "mam bul dupy", "mam durzo pracy"],
+    naczym = ["na tapczanie", "na klawiaturze", "na wersalce", "na kutasie", "na kontenerze", "na lotnisku krakuw balice", "na umowie cywilnej", "na umowie o dzieuo", "na notatniku", "na papierosku", "na gugle mapsie", "na serwerze pocztowym", "na bazie teleadresowej", "na telefonie", "na wysypisku odpaduw", "na odpierdol", "na taśmie do produkcji", "na kościelnej ławie", "na wybrzerzu helskim", "na wybrzerzu gdanskim niedaleko stoczni", "na kursorze w komputerze", "na pasku zadań", "na wczasach", "na kursie java skryptu", "na zajenciach z polonistyki", "na dyskotece fajnej", "na śmiesznej herbacie", "na wysypisku guwna", "na urlopie wypoczynkowym", "na wyjściu do sklepu", "na podkładce pod myszke", "na telewizorze", "na fotelu", "na lerzaku", "na antresoli", "na mieście", "na rybach", "na jajcach", "na niczym ciekawym", "na koniu jadąc", "na pokładzie samolotu", "na worku złota", "na kocim grzbiecie", "na ikonie muj komputer", "na koncercie", "na wycieczce", "na piwku", "na wylocie", "na krześle", "na żarty", "na temblaku", "na posterunku", "na komendzie policji"],
+    jakco = ["jak jakiś skrót", "jak galareta", "jak jakiś chuj", "jak jakaś ryba", "jak jakieś jajo", "jak jakaś herbata czarna", "jak jakiś kalkulator", "jak dziki pies", "jak jakiś bobas", "jak kurczak", "jak pan policjant", "jak cienrzaruwka na granicy", "jak jakiś piwosz", "jak jakiś suoik", "jak nie wiem co", "jak jakiś debil", "jak kursor na folderze", "jak mikroprocesor", "jak bezrobotny", "jak jakiś glut", "jakoś ostatkiem sił", "jak puszka piwa", "jak jakiś napis albo obrazek", "jak okrent morski", "jak zimna fala loduwki", "jak ciepua fala kuchenki", "jak jakaś picca", "jak kebab", "jak jakiś jubilat", "jak pieczywo w torbie", "jak trujoncy grzyb", "jak niebezpieczny wonrz", "jak donald tramp", "jak stary anona", "jak jan papiesz", "jak papierosek ciepły", "jak smaczny obiad", "jak pan lis", "jak byle co", "jak ciapaja", "jak rarug", "jak szafeczka", "jak jakiś interfejs", "jak android", "jak jakaś pani w sklepie", "jak pieniondz w portfelu"],
+    nazywamsie = ["moja godność to", "mam na imie", "nazywam sie", "posuchaj ja nazywam sie", "muj gatunek to", "ten awatar nazywa sie", "moja klasa obiektu to", "dzieci muwiom rze jestem", "w dowodzie mam wpisane", "muwiom do mnie", "moja nazwa usługi to", "moja data warzności to", "nazwa tego pliku", "karzdy katolik to", "godność moja to", "moje imie i nazwisko brzmi", "moja nazwa to", "moja domenta to", "moja marka to", "muj znak firmowy to", "muj adres emajl to", "nazywam sie", "ja nazywam sie", "muw do mnie", "ja nazywam sie", "mam na nazwisko", "muj adres to"],
+    nazwa = ["ciapaja", "cichy nabiau", "guwno", "karabinek", "jeremiasz", "ojciec jeremiasz", "mieszadełko", "pozdrowionka", "ściskacz", "telefon sony xperia", "karta pamięci", "ciągniczek", "ciepła zupka i zimne spodnie", "sopel lodu", "uty do tańca", "kolorowy balonik", "piotrek", "leszek", "ukasz", "kinga", "agnieszka", "julka", "wojtek", "chlebek", "smutny chlebek", "bułeczka z serkiem", "traktorek", "korporacja finansowa", "polska firma", "platforma android", "bluza z kapturem", "elektroodpady", "robinson krauze", "zakuad ubezpieczeń", "wampir", "urzytkownik fejsbuka", "adresat poczty", "nadawca przesyłki", "rower gurski", "zły pies", "zwionzek radziecki", "karabin maszynowy", "katolik polski", "stary gajowy", "twuj komputer", "moja mama", "fasola czerwona", "czekoladka", "cukiereczek", "siusiaczek", "dupeczka", "okienko", "miska z ryrzem", "ruski agent", "plastusiowy pamientnik", "nasza klasa", "piosenkarz polski", "kobieta pracujonca", "systemowy buond", "piotrek pucieniczak", "krul polski", "wioleta wilas", "statusiarz jeden", "poszukiwany bandyta", "niebezpieczny pajonk", "głodna osa"],
+    wczym = ["w majonezie", "w sklepie sporzywczym", "w dupie", "w korytarzu", "w budce wikinga", "w ziemniaku", "w garnku ziemniakuw", "w ponurej okolicy", "w czasie", "w egipskim kurorcie", "w rosji putinoskiej", "w twojej guowie", "w twoim domu", "w morzu bałtyckim", "u adama i martyny", "w krakowie", "w bibliotece naukowej", "w przychodni lekarskiej", "w chlewie jakimś", "w samolocie do polski", "w pociongu osobowym", "w niczym", "w kosmosie", "w obozie uchodzcuw", "w samochodzie", "w tajdze", "w lesie", "w gradiencie", "w galeri sztuki", "w ciemnej ulicy", "w wojewudztwie pomorskim", "w kopalni głemboko", "w pamienci ram komputera", "w ksionrzce do historii", "w ramach prawa", "w przestrzeni", "w jenzyku polskim", "w podrurzy", "w trakcie antraktu", "w czasie koncertu", "w czystym łurzku", "w łurzku z tobom", "w dawnej stolicy", "w czasie suszy", "w moim pokoju"],
+    jestem = ["przebywam obecnie", "spendzam czas", "jestem zameldowany", "zaznaczam okienko", "zarzywam urlopu", "mieszkam", "jem obiady", "buduje domek", "szczelam z pistoletu", "mam zameldowanie", "mam zgode na pobyt", "mam adres", "mam adres zameldowania", "mam zezwolenie na pobyt", "mam karte mieszkańca", "mam mieszkanie", "przebywam obecnie", "jestem teras", "jestem", "rzyje", "rzyje i mam sie dobrze", "przebywam teraz", "odbywam wyrok", "egzystuje"],
+    czesc = ["cześć", "witam ciepło", "witam serdecznie", "dzień dobry", "dobry wieczur", "prosze zapraszam", "dobry dzień", "szanowni goście", "cześć witajcie", "witam", "jak sie macie", "no cześć", "hej", "no witam", "witam tutaj", "prosze siadać", "no hejka", "halo", "dzien dobry", "hej hej"];
 
 var losujCos = function (n) {
         return Math.floor(Math.random() * (n));
@@ -125,19 +125,25 @@ var generator = function () {
         czescMax = czesc.length,
         losczesc = losujCos(czescMax),
         // losuj z liczby case'ów - 1
-        coBedzie = losujCos(4);
+        coBedzie = losujCos(6);
     switch (coBedzie) {
     case 0:
-        status = jestem[losjestem] + " " + wczym[loswczym] + ". ";
+        status = robie[losrobie] + " " + jakco[losjakco];
         break;
-    case 1:
-        status = czesc[losczesc] + ", " + nazywamsie[losnazywamsie] + " " + nazwa[losnazwa] + ". ";
+        case 1:
+        status = robie[losrobie] + " " + jakco[losjakco] + " " + naczym[losnaczym];
         break;
-    case 2:
-        status = nazywamsie[losnazywamsie] + " " + nazwa[losnazwa] + ", " + robie[losrobie] + " " + naczym[losnaczym] + ". ";
+        case 2:
+        status = nazywamsie[losnazywamsie] + " " + nazwa[losnazwa];
         break;
-    case 3:
-        status = robie[losrobie] + " " + naczym[losnaczym] + " " + jakco[losjakco] + ". ";
+        case 3:
+        status = czesc[losczesc] + " " + nazywamsie[losnazywamsie] + " " + nazwa[losnazwa];
+        break;
+        case 4:
+        status = czesc[losczesc] + " " + jestem[losjestem] + " " + wczym[loswczym];
+        break;
+        case 5:
+        status = jestem[losjestem] + " " + jakco[losjakco];
         break;
     }
     $("#generator").html(status);
@@ -145,7 +151,7 @@ var generator = function () {
 
 // powiedz
 var powiedz = function (status) {
-    meSpeak.speak(status);
+    meSpeak.speak(status, {speed: 150});
 }
 
 // funcja główna
@@ -161,7 +167,7 @@ var nabialator = function () {
         powiedz(status);
         // setTimeout(powiedz, 1000);
         // zacznij od nowa
-        setTimeout(nabialator, 5000);
+        setTimeout(nabialator, 6000);
     };
 
 $(document).ready(function () {
